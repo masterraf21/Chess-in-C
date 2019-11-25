a3ee6e9afe304ff9edbf66ec3f8429947fec52f8
ShafaAM <shfamrsya@gmail.com>
2019-11-25 21:33:38 +0700

    update selektor queuegiliran

--- src/adt/queuegiliran.c
+++ src/adt/queuegiliran.c
@@ -2,12 +2,7 @@
 #include <stdio.h>
 #include "queuegiliran.h"
 
-/* Prototype manajemen memori */
 void AlokasiTurn (address *P, infotypeturn X)
-/* I.S. Sembarang */
-/* F.S. Alamat P dialokasi, jika berhasil maka InfoT(P)=X dan 
-        NextT(P)=Nil */
-/*      P=Nil jika alokasi gagal */
 {
 	*P = (address) malloc(sizeof(ElmtQueueTurn));
 
@@ -17,18 +12,14 @@ void AlokasiTurn (address *P, infotypeturn X)
 	}
 }
 void DealokasiTurn (address  P)
-/* I.S. P adalah hasil alokasi, P != Nil */
-/* F.S. Alamat P didealokasi, dikembalikan ke sistem */
 {
 	free(P);
 }
 boolean IsEmptyTurn (Queue Q)
-/* Mengirim true jika Q kosong: HEAD(Q)=Nil and TAIL(Q)=Nil */
 {
 	return ((HeadT(Q)==Nil)&&(TailT(Q)==Nil));
 }
 int NbElmtTurn(Queue Q)
-/* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika Q kosong */
 {
 	int count = 0;
 	address P = HeadT(Q);
@@ -40,21 +31,13 @@ int NbElmtTurn(Queue Q)
 	
 	return count;
 }
-/*** Kreator ***/
+
 void CreateEmptyTurn(Queue * Q)
-/* I.S. sembarang */
-/* F.S. Sebuah Q kosong terbentuk */
 {
 	HeadT(*Q) = Nil;
 	TailT(*Q) = Nil;
 }
-/*** Primitif AddTurn/DelTurnete ***/
 void AddTurn (Queue * Q, infotypeturn X)
-/* Proses: Mengalokasi X dan menambahkan X pada bagian TAIL dari Q
-   jika alokasi berhasil; jika alokasi gagal Q tetap */
-/* Pada dasarnya adalah proses insert last */
-/* I.S. Q mungkin kosong */
-/* F.S. X menjadi TAIL, TAIL "maju" */
 {
 	address P;
 	AlokasiTurn(&P, X);
@@ -72,11 +55,6 @@ void AddTurn (Queue * Q, infotypeturn X)
 	}
 }
 void DelTurn(Queue * Q, infotypeturn * X)
-/* Proses: Menghapus X pada bagian HEAD dari Q dan mendealokasi
-   elemen HEAD */
-/* Pada dasarnya operasi delete first */
-/* I.S. Q tidak mungkin kosong */
-/* F.S. X = nilai elemen HEAD pd I.S., HEAD "mundur" */
 {
 	address P = HeadT(*Q);
 
--- src/adt/queuegiliran.h
+++ src/adt/queuegiliran.h
@@ -20,54 +20,54 @@ typedef struct {
 typedef struct tElmtQueueTurn * address;
 typedef struct tElmtQueueTurn { 
   infotypeturn InfoT;
-  address Next; 
+  address NextT; 
 } ElmtQueueTurn; 
 
 /* Type queue dengan ciri HEAD dan TAIL : */
 typedef struct { 
-  address HEAD;  /* alamat penghapusan */
-  address TAIL;  /* alamat penambahan */
+  address HEADT;  /* alamat penghapusan */
+  address TAILT;  /* alamat penambahan */
 } Queue_Giliran;
 
 typedef Queue_Giliran Queue;
 
 /* Selektor */
-#define HeadT(Q) (Q).HEAD
-#define TailT(Q) (Q).TAIL
-#define InfoHeadT(Q) (Q).HEAD->Info
-#define InfoTailT(Q) (Q).TAIL->Info
-#define NextT(P) (P)->Next
-#define InfoT(P) (P)->Info
+#define HeadT(Q) (Q).HEADT
+#define TailT(Q) (Q).TAILT
+#define InfoHeadT(Q) (Q).HEAD->InfoT
+#define InfoTailT(Q) (Q).TAIL->InfoT
+#define NextT(P) (P)->NextT
+#define InfoT(P) (P)->InfoT
 
 /* Prototype manajemen memori */
 void AlokasiTurn (address *P, infotypeturn X);
 /* I.S. Sembarang */
-/* F.S. Alamat P dialokasi, jika berhasil maka Info(TP)=X dan 
-        Next(P)=Nil */
+/* F.S. Alamat P dialokasi, jika berhasil maka InfoT(P)=X dan 
+        NextT(P)=Nil */
 /*      P=Nil jika alokasi gagal */
 void DealokasiTurn (address  P);
 /* I.S. P adalah hasil alokasi, P != Nil */
 /* F.S. Alamat P didealokasi, dikembalikan ke sistem */
 boolean IsEmptyTurn (Queue Q);
-/* Mengirim true jika Q kosong: HEAD(Q)=Nil and TAIL(Q)=Nil */
+/* Mengirim true jika Q kosong: HEADT(Q)=Nil and TAILT(Q)=Nil */
 int NbElmtTurn(Queue Q);
 /* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika Q kosong */
 /*** Kreator ***/
 void CreateEmptyTurn(Queue * Q);
 /* I.S. sembarang */
 /* F.S. Sebuah Q kosong terbentuk */
-/*** Primitif AddTurn/DelTurnete ***/
+/*** Primitif AddTurn/Delete ***/
 void AddTurn (Queue * Q, infotypeturn X);
-/* Proses: Mengalokasi X dan menambahkan X pada bagian TAIL dari Q
+/* Proses: Mengalokasi X dan menambahkan X pada bagian TAILT dari Q
    jika alokasi berhasil; jika alokasi gagal Q tetap */
 /* Pada dasarnya adalah proses insert last */
 /* I.S. Q mungkin kosong */
-/* F.S. X menjadi TAIL, TAIL "maju" */
+/* F.S. X menjadi TAILT, TAILT "maju" */
 void DelTurn(Queue * Q, infotypeturn * X);
-/* Proses: Menghapus X pada bagian HEAD dari Q dan mendealokasi
-   elemen HEAD */
+/* Proses: Menghapus X pada bagian HEADT dari Q dan mendealokasi
+   elemen HEADT */
 /* Pada dasarnya operasi delete first */
 /* I.S. Q tidak mungkin kosong */
-/* F.S. X = nilai elemen HEAD pd I.S., HEAD "mundur" */
+/* F.S. X = nilai elemen HEADT pd I.S., HEADT "mundur" */
 
 #endif
